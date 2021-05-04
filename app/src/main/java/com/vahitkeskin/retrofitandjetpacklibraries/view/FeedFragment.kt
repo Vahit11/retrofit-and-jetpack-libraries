d79597cc1202ea29ee9d878f002fe2faa8cf01f6
Vahit Keskin <36104238+Vahit11@users.noreply.github.com>
2021-05-04 20:46:01 +0300

    vahitkeskin

--- app/src/main/java/com/vahitkeskin/retrofitandjetpacklibraries/view/FeedFragment.kt
+++ app/src/main/java/com/vahitkeskin/retrofitandjetpacklibraries/view/FeedFragment.kt
@@ -5,19 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.vahitkeskin.retrofitandjetpacklibraries.R
+import com.vahitkeskin.retrofitandjetpacklibraries.databinding.FragmentFeedBinding
 
-class FeedFragment : Fragment() {
+class FeedFragment : Fragment(R.layout.fragment_feed) {
+
+    private lateinit var binding: FragmentFeedBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentFeedBinding.bind(view)
+        binding.fab.setOnClickListener {
+            Toast.makeText(context,"Click floating button",Toast.LENGTH_LONG).show()
+        }
     }
 }
