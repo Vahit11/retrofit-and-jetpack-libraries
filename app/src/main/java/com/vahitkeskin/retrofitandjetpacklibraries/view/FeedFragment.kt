82e4f304b00ba7a01960f2261894f081206e4fa6
Vahit Keskin <36104238+Vahit11@users.noreply.github.com>
2021-05-04 22:36:06 +0300

    vahitkeskin

--- app/src/main/java/com/vahitkeskin/retrofitandjetpacklibraries/view/FeedFragment.kt
+++ app/src/main/java/com/vahitkeskin/retrofitandjetpacklibraries/view/FeedFragment.kt
@@ -2,21 +2,14 @@ package com.vahitkeskin.retrofitandjetpacklibraries.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import com.vahitkeskin.retrofitandjetpacklibraries.R
 import com.vahitkeskin.retrofitandjetpacklibraries.databinding.FragmentFeedBinding
-import kotlinx.coroutines.delay
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
 
     private lateinit var binding: FragmentFeedBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,9 +17,7 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
         binding = FragmentFeedBinding.bind(view)
         binding.fab.setOnClickListener {
             Thread.sleep(1000)
-            Toast.makeText(context,"Click floating button",Toast.LENGTH_LONG).show()
-            Toast.makeText(context,"Click floating button",Toast.LENGTH_LONG).show()
-            println("Vahit Keskin")
+            Toast.makeText(context, "Click floating button", Toast.LENGTH_LONG).show()
         }
     }
 }
